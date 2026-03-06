48a249fdee1a4a874caad82d1f08ec25a038c58d
tubplunger <143738688+tubplunger@users.noreply.github.com>
2026-03-06 12:49:50 -0800

    Art and Sound
    Art and sound have been added to the game, the player now being a basket and a sound playing with each ball caught.

--- src/Catch/Assets/Scripts/BallCatcher.cs
+++ src/Catch/Assets/Scripts/BallCatcher.cs
@@ -4,11 +4,20 @@ using UnityEngine;
 
 public class BallCatcher : MonoBehaviour
 {
+    private AudioSource audioSource;
+
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Ball"))
         {
             GameManager.Instance.AddScore(1);
+
+            audioSource.Play();
 
             Destroy(other.gameObject);
         }
